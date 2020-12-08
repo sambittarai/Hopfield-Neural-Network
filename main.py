e9a398a118ae6b69e119e0b8c5056d9e06d09b44
Sambit Tarai <sambitarai17@gmail.com>
2020-12-08 18:06:51 +0530

    main file updated

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#main
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from sklearn.metrics import mean_squared_error
+
+def convert_txt_to_array(path, arr):
+	'''
+	path - path of the text file
+	arr - matrix with appropriate shape as that of the text file
+
+	returns a numpy array with the given pixel values in the text file
+	'''
+	file = open(path, "r")
+	z = 0
+	for line in file:
+		arr[z, :] = np.array(line.split(',')).astype('float')
+		z += 1
+
+	return arr
+
