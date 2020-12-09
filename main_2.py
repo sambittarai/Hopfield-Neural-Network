6d9e4ae4f9ca9347c777efd5263e7698edac607f
Sambit Tarai <sambitarai17@gmail.com>
2020-12-09 12:56:32 +0530

    main_2 updated

--- main_2.py
+++ main_2.py
@@ -25,39 +25,38 @@ def calc_weight(epsilon):
 	return W
 
 def main():
-  #path_ball = "/content/drive/MyDrive/Computational Neuroscience/ball.txt"
-  path_ball = input("Enter the path of the text file of the image")
-  ball_arr = convert_txt_to_array(path_ball, np.zeros((90, 100)))
+	#path_ball = "/content/drive/MyDrive/Computational Neuroscience/ball.txt"
+	path_ball = input("Enter the path of the text file of the image")
+	ball_arr = convert_txt_to_array(path_ball, np.zeros((90, 100)))
 
-  mask_image = np.zeros((90, 100))
-  mask_image[:35,20:55] = ball_arr[:35,20:55]
-  N = ball_arr.shape[0]*ball_arr.shape[1] #No. of neurons
-  NO_OF_ITERATIONS = 8
-  epsilon1 = ball_arr.reshape(1, N)
-  test_array = mask_image.reshape(1, N)
+	mask_image = np.zeros((90, 100))
+	mask_image[:35,20:55] = ball_arr[:35,20:55]
+	N = ball_arr.shape[0]*ball_arr.shape[1] #No. of neurons
+	NO_OF_ITERATIONS = 8
+	epsilon1 = ball_arr.reshape(1, N)
+	test_array = mask_image.reshape(1, N)
 
-  W = calc_weight(epsilon1) #Weight Matrix
-  h = np.zeros((N))
-  rms = np.zeros((NO_OF_ITERATIONS))
+	W = calc_weight(epsilon1) #Weight Matrix
+	h = np.zeros((N))
+	rms = np.zeros((NO_OF_ITERATIONS))
 
-  for iteration in tqdm(range(NO_OF_ITERATIONS)):
-      for i in range(N):
-          i = np.random.randint(N)
-          h[i] = 0
-          for j in range(N):
-              h[i] = W[i, j]*test_array[0, j]
-      test_array = (np.where(h<0, -1, 1)).reshape(1, N)
-      rms[iteration] = mean_squared_error(test_array, ball_arr.reshape(1, N), squared=False)
-      plt.imshow(np.where(test_array.reshape(90,100)<0, -1, 1), cmap='gray')
-      plt.show()
+	for iteration in tqdm(range(NO_OF_ITERATIONS)):
+	    for i in range(N):
+	        i = np.random.randint(N)
+	        h[i] = 0
+	        for j in range(N):
+	            h[i] = W[i, j]*test_array[0, j]
+	    test_array = (np.where(h<0, -1, 1)).reshape(1, N)
+	    rms[iteration] = mean_squared_error(test_array, ball_arr.reshape(1, N), squared=False)
+	    plt.imshow(np.where(test_array.reshape(90,100)<0, -1, 1), cmap='gray')
+	    plt.show()
 
-
-  #plot the root mean squared error vs time
-  plt.plot(np.arange(NO_OF_ITERATIONS), rms)
-  plt.xlabel('Time')
-  plt.ylabel('RMS error')
-  plt.title('RMS error vs Time')
-  plt.show()
+	#plot the root mean squared error vs time
+	plt.plot(np.arange(NO_OF_ITERATIONS), rms)
+	plt.xlabel('Time')
+	plt.ylabel('RMS error')
+	plt.title('RMS error vs Time')
+	plt.show()
 
 
 main()
